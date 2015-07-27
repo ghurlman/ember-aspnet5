31eb5a4cbfac46f22847de1a25b506cab94cda48
Greg Hurlman <grhurl@microsoft.com>
2015-07-27 08:50:07 -0400

    project files

---
+++ src/ember-test/gulpfile.js
@@ -0,0 +1,12 @@
+﻿/// <binding ProjectOpened='watch' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var shell = require('gulp-shell')
+
+gulp.task('build', shell.task('ember build'));
+
+gulp.task('watch', shell.task('ember build -w'));
